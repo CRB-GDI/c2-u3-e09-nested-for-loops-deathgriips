22ee7cdb224227733c663d3043360e72d8bad492
adam ^.^ <145371242+deathgriips@users.noreply.github.com>
2024-02-29 13:21:07 -0500

    Finished problem 1

---
+++ Exercise9/Exercise9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Problem 1
+
+            Console.Write("Enter row width: ");
+            int rowWidth = int.Parse(Console.ReadLine());
+            Console.Write("Enter number of rows: ");
+            int rowAmount = int.Parse(Console.ReadLine());
+
+            // Create nested for loop
+            for (int i = 1; i < rowAmount; i++)
+            {
+                for (int j = 1; j < rowWidth; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
